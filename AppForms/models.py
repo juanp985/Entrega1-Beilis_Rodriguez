e178997a1337ec8310d7ab814c2c43872027322a
Juan Beilis <juanp985@gmail.com>
2022-06-07 11:08:39 -0300

    final

--- AppForms/models.py
+++ AppForms/models.py
@@ -5,11 +5,19 @@ class Curso(models.Model):
     nombre=models.CharField(max_length=40)
     comision = models.IntegerField()
 
+class Meta:
+        verbose_name = 'curso'
+        verbose_name_plural = 'cursos'
+
 class Estudiante(models.Model):
     legajo = models.IntegerField(unique=True)
     nombre= models.CharField(max_length=30)
     apellido= models.CharField(max_length=30)
     email= models.EmailField()
+
+class Meta:
+        verbose_name = 'estudiante'
+        verbose_name_plural = 'estudiantes'
 
 class Profesor(models.Model):
     nombre= models.CharField(max_length=30)
@@ -17,6 +25,9 @@ class Profesor(models.Model):
     email= models.EmailField()
     profesion= models.CharField(max_length=30)
 
+class Meta:
+        verbose_name = 'profesor'
+        verbose_name_plural = 'profesores'
 
 
 
